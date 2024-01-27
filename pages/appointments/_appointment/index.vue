68c6d27cc5b8346bab94b597762e154d6ac50b43
Mehmet Koçer <mehmet.kocer@despatchcloud.com>
2024-01-27 15:11:54 +0300

    feat: appointment detail page added, appointment update methods added

--- pages/appointments/_appointment/index.vue
+++ pages/appointments/_appointment/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page">
-    <h1>Randevu Detay</h1>
-    {{ apppointment }}
-    <div class="page__form">
-      <AppointmentForm />
+    <h1>Randevu Oluştur</h1>
+    <p v-if="$fetchState.pending">Fetching appointment...</p>
+    <div v-else class="page__form">
+      <AppointmentForm v-model="data" @submit="handleSubmit" />
     </div>
   </div>
 </template>
@@ -17,6 +17,17 @@ export default {
   data() {
     return {
       apppointment: null,
+      data: {
+        appointment_id: null,
+        appointment_date: null,
+        appointment_postcode: null,
+        agent_id: null,
+        contact_id: null,
+        contact_name: null,
+        contact_surname: null,
+        contact_email: null,
+        contact_phone: null,
+      },
     }
   },
   async fetch() {
@@ -29,8 +40,62 @@ export default {
         const response = await this.$api.$get(
           `/tblHgSMxTwolE6MeC/${appointmentId}`
         )
-
-        this.apppointment = response
+        this.data.appointment_id = response.id
+        this.data.appointment_date = new Date(response.fields.appointment_date)
+        this.data.appointment_postcode = response.fields.appointment_postcode
+        this.data.agent_id = response.fields.agent_id?.[0]
+        this.data.agent_name = response.fields.agent_name?.[0]
+        this.data.agent_surname = response.fields.agent_surname?.[0]
+        this.data.contact_id = response.fields.contact_id?.[0]
+        this.data.contact_name = response.fields.contact_name?.[0]
+        this.data.contact_surname = response.fields.contact_surname?.[0]
+        this.data.contact_email = response.fields.contact_email?.[0]
+        this.data.contact_phone = response.fields.contact_phone?.[0]
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async handleSubmit() {
+      try {
+        await this.updateContact(this.data)
+        await this.updateAppointment(this.data)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async updateContact(data) {
+      try {
+        const requestData = {
+          contact_name: data.contact_name,
+          contact_surname: data.contact_surname,
+          contact_email: data.contact_email,
+          contact_phone: data.contact_phone,
+        }
+        const contact = await this.$api.$patch(
+          `tblTSgRmykDmvOMJF/${this.data.contact_id}`,
+          {
+            fields: requestData,
+          }
+        )
+        this.data.contact_id = contact.id
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async updateAppointment(data) {
+      try {
+        const requestData = {
+          appointment_date: data.appointment_date,
+          appointment_postcode: data.appointment_postcode,
+          agent_id: [data.agent_id],
+          contact_id: [data.contact_id],
+        }
+        await this.$api.$patch(
+          `tblHgSMxTwolE6MeC/${this.data.appointment_id}`,
+          {
+            fields: requestData,
+          }
+        )
       } catch (error) {
         console.log(error)
       }
